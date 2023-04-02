3709a48c2beb48351740f136c3a0ceca26d9b7d4
Matheus Silva <matheus.silva.contato26@gmail.com>
2023-04-02 19:51:13 -0300

    first commit project presentation

---
+++ src/js/index.js
@@ -0,0 +1,33 @@
+const botoesCarrossel = document.querySelectorAll('.botao');
+const imagens = document.querySelectorAll('.imagem');
+
+botoesCarrossel.forEach((botao, indice) => {
+    botao.addEventListener('click', () => {
+        desativarBotaoSlecionado();
+
+        selecionarBotaoCarrossel(botao);
+
+        esconderImagemAtiva();
+
+        mostrarImagemDeFundo(indice);
+
+    })
+})
+
+function mostrarImagemDeFundo(indice) {
+    imagens[indice].classList.add('ativa');
+}
+
+function selecionarBotaoCarrossel(botao) {
+    botao.classList.add('selecionado');
+}
+
+function esconderImagemAtiva() {
+    const imagemAtiva = document.querySelector('.ativa');
+    imagemAtiva.classList.remove('ativa');
+}
+
+function desativarBotaoSlecionado() {
+    const botaoSelecionado = document.querySelector('.selecionado');
+    botaoSelecionado.classList.remove('selecionado');
+}
